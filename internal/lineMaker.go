336ba2ab0de628cd87f774a9978d19c00911a578
elmehdibelfkih <elmehdibelfkih@gmail.com>
2024-12-29 12:39:08 +0100

    quote handler: done, stable version v2

--- internal/lineMaker.go
+++ internal/lineMaker.go
@@ -2,8 +2,8 @@ package internal
 
 import (
 	"bufio"
-	"fmt"
 	"go-reloaded/pkg"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -17,6 +17,7 @@ func HandelLine(input *os.File, output *os.File) {
 		line = binHexHandler(line, "(hex)", 16)
 		line = orderReplace(line)
 		line = punctuationsHandler(line)
+		line = quoteHandler(line)
 		output.WriteString(line + "\n")
 	}
 }
@@ -50,6 +51,55 @@ func flagHandler(line string, index int, mode string, opp func(string) string) s
 	return line
 }
 
-func punctuationsHandler(line string) string { //FIXME:
-	return line
+func punctuationsHandler(line string) string {
+	var ret string
+	var isPunctuation bool
+
+	for _, c := range line {
+		if strings.Contains(`.!?,:;`, string(c)) {
+			ret = strings.TrimRight(ret, " ")
+			ret += string(c) + " "
+			isPunctuation = true
+		} else {
+			if c == ' ' && isPunctuation {
+				continue
+			} else {
+				ret += string(c)
+				isPunctuation = false
+			}
+		}
+	}
+	return ret
+}
+
+func quoteHandler(line string) string {
+	var ret string
+	var firstQuote bool
+
+	quoteCounter := strings.Count(line, `'`)
+	if quoteCounter < 2 {
+		return line
+	}
+	if quoteCounter%2 != 0 {
+		quoteCounter--
+	}
+	for _, c := range line {
+		if c == '\'' && quoteCounter%2 == 0 && quoteCounter != 0{
+			firstQuote = true
+			ret += string(c)
+			quoteCounter--
+		} else if c == ' ' && firstQuote {
+			continue
+		} else if firstQuote {
+			ret += string(c)
+			firstQuote = false
+		} else if c == '\'' && quoteCounter%2 != 0 && quoteCounter != 0{
+			ret = strings.TrimRight(ret, " ")
+			ret += "'"
+			quoteCounter--
+		} else {
+			ret += string(c)
+		}
+	}
+	return ret
 }
